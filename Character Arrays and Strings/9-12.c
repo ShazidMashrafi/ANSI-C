bcfe598698b9d313d5e275bc281e79e8f221748b
ShazidMahsrafi <shazid456@gmail.com>
2023-05-21 01:04:20 +0600

    initial commit

---
+++ Character Arrays and Strings/9-12.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[100],substr[10];
+    printf("Enter a line of text: ");
+    gets(str);
+    printf("Enter substring: ");
+    gets(substr);
+    char *ptr=strstr(str,substr);
+    
+    int count=0;
+    while(ptr!=NULL)
+    {
+        count++;
+        ptr=strstr(ptr+1,substr);
+    }
+    printf("The number of occurences of substring \"%s\" in the line of text \"%s\" is %d",substr,str,count);
+    return 0;
+}
